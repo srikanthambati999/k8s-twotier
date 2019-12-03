203029eb3e53a836fb6c2981315cf286238d263a
srikanthambati999 <49298862+srikanthambati999@users.noreply.github.com>
2019-12-03 23:11:07 +0530

    Update UserController.java

--- src/main/java/com/mt/springmongo/UserController.java
+++ src/main/java/com/mt/springmongo/UserController.java
@@ -5,11 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- * Controller which handles reqest for saving {@link User}s.
- *
- * @author Mithun
- */
+
 @Controller
 public class UserController {
     private final UserRepository userRepository;
